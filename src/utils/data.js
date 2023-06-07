2d416ab45bbe94c8be6844dfba9b4436868d988c
Dev-scott <sadoscott25@gmail.com>
2023-06-07 06:06:58 +0200

    update

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,3 +4,26 @@ export const startingFeatures = [
     'No need to beat around the bush, just stay on the gas and have fun',
   ];
   
+
+  export const socials = [
+
+
+    {
+        name: 'discord',
+        url: '/public/img/discorde.png',
+      },
+      {
+        name: 'github',
+        url: '/public/img/logo-github.png',
+      },
+      {
+        name: 'youtube',
+        url: '/public/img/youtube.png',
+      },
+      {
+        name: 'twitter',
+        url: '/public/img/twitter.png',
+      },
+    
+
+  ]
